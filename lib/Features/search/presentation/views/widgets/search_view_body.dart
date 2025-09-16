986ae7e6e8f9d71bc7a809546d4799130aa0399e
mohamedELazab111 <mobr876@gmail.com>
2025-09-16 23:28:54 +0300

    search feature part 2

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/Features/home/presentation/views/widgets/custom_text_field.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: []);
+    return const Column(
+      children: [
+        CustomTextField(hint: 'Search'),
+        Text('Enter the note title'),
+      ],
+    );
   }
 }
